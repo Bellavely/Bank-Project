2b5a9584d3802b0582ed84296ac1ee1010910da6
bella.vayzburg <bella.vaisborg@gmail.com>
2026-04-27 22:10:29 +0300

    added wallet logic

--- apps/cashio-backend/dal/index.ts
+++ apps/cashio-backend/dal/index.ts
@@ -1,6 +1,3 @@
-export * from './auth'
-export * from './users'
-
-export function getBalance(userId: number) {
-    throw new Error("Function not implemented.")
-}
+export * from "./auth";
+export * from "./users";
+export * from "./wallet";
