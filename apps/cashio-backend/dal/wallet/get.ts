40ce14489b4c804f6f8339301b1e0f1ce6a6c911
bella.vayzburg <bella.vaisborg@gmail.com>
2026-04-29 16:25:35 +0300

    added wallet type

--- apps/cashio-backend/dal/wallet/get.ts
+++ apps/cashio-backend/dal/wallet/get.ts
@@ -1,7 +1,8 @@
 import { walletCollection } from "../../models";
 import mongoose from "mongoose";
+import { Wallet } from "../consts";
 
 export const getBalance = async (userId: string) => {
   const userIdObject = new mongoose.Types.ObjectId(userId);
-  return walletCollection.findOne({ userId: userIdObject }).lean();
+  return walletCollection.findOne({ userId: userIdObject }).lean<Wallet>();
 };
--- apps/cashio-backend/dal/wallet/post.ts
+++
@@ -1,8 +0,0 @@
-import { walletCollection } from "../../models";
-
-export const updateUsersBalance = async (userId: string, amount: number) => {
-  await walletCollection.updateOne(
-    { userId, balance: { $gte: amount } },
-    { $inc: { balance: amount } },
-  );
-};
