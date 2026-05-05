6aa65fa8c3900594d61554eb210e8e998bb6a90b
bella.vayzburg <bella.vaisborg@gmail.com>
2026-05-05 13:19:03 +0300

    add mock pages

--- apps/cashio-fronted/src/pages/Dashboard/Dashboard.tsx
+++ apps/cashio-fronted/src/pages/Dashboard/Dashboard.tsx
@@ -1,3 +1,71 @@
+import { useState } from "react";
+import styles from "./dashboard.module.css";
+const mockTransactions = [
+  {
+    id: 1,
+    type: "הפקדה",
+    amount: 100,
+    from: "משתמש 1",
+    date: "18.4.2026",
+  },
+  {
+    id: 2,
+    type: "הוצאה",
+    amount: 100,
+    to: "משתמש 2",
+    date: "18.4.2026",
+  },
+];
+
 export default function Dashboard() {
-  return <div>dashboard</div>;
+  const [balance] = useState(20000);
+  const [search, setSearch] = useState("");
+  const [visibleCount, setVisibleCount] = useState(5);
+  const filtered = mockTransactions.filter((t) =>
+    JSON.stringify(t).toLowerCase().includes(search.toLowerCase()),
+  );
+  const visibleTransactions = filtered.slice(0, visibleCount);
+
+  return (
+    <div className={styles["page"]}>
+      <div className={styles["card"]}>
+        <h2 className={styles["title"]}>היתרה שלך:</h2>
+        <div className={styles["balance"]}>₪ {balance.toLocaleString()}</div>
+      </div>
+      <div className={styles["actions"]}>
+        <button className={styles["circleBtn"]}>↑ שלח</button>
+      </div>
+
+      <input
+        className={styles["search"]}
+        placeholder="חפש"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <div className={styles["list"]}>
+        {visibleTransactions.map((t) => (
+          <div key={t.id} className={styles["item"]}>
+            <div className={styles["amount"]}>
+              {t.type === "הפקדה" ? "+" : "-"} {t.amount}
+            </div>
+            <div className={styles["meta"]}>
+              <div>{t.from || t.to}</div>
+              <div>{t.type} </div>
+              <div className={styles.date}>{t.date}</div>
+            </div>
+          </div>
+        ))}
+        {visibleCount < filtered.length && (
+          <button
+            className={styles["loadMore"]}
+            onClick={() => setVisibleCount((prev) => prev + 5)}
+          >
+            טען עוד
+          </button>
+        )}
+      </div>
+
+      {filtered.length === 0 && <div>אין תוצאות</div>}
+    </div>
+  );
 }
--- apps/cashio-fronted/src/pages/sendMoney/index.ts
+++ apps/cashio-fronted/src/pages/sendMoney/index.ts
@@ -1,1 +1,1 @@
-export * from "./SendMoney";
+export * from "./TransferPage";
