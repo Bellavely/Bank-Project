ebd89cf1b130964fce6ba4751b5eafad9ea5be10
bella vayzburg <bella.vaisborg@gmail.com>
2026-06-10 23:22:37 +0300

    more changes for deployment

--- apps/cashio-backend/prisma/prismaClient.ts
+++ apps/cashio-backend/prisma/prismaClient.ts
@@ -1,7 +1,11 @@
 import "dotenv/config";
+import pg from "pg";
 import { PrismaClient } from "./generated/client/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
-const connectionString = `${process.env.DATABASE_URL}`;
-const adapter = new PrismaPg(connectionString);
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const adapter = new PrismaPg(pool);
 export const prisma = new PrismaClient({ adapter });
